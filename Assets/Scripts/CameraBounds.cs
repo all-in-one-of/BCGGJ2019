f249cbf7f26338e4fbc50180ae70ccedc8c26b5f
jcaracciolo <jcaracciolo@itba.edu.ar>
2019-01-26 03:55:21 -0300

    Added side movement

---
+++ Assets/Scripts/CameraBounds.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBounds : MonoBehaviour {
+
+	void OnDrawGizmosSelected()
+	{
+		// Draw a semitransparent blue cube at the transforms position
+		var bounds = GetComponent<Camera>().NominalScreenAt(Vector3.zero);
+		Gizmos.color = Color.red;
+		Gizmos.DrawCube(bounds.center, bounds.extents);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public static class CameraExtensions
+{
+	public static Bounds OrthographicBounds(this Camera camera, Transform transform)
+	{
+		float screenAspect = (float)Screen.width / (float)Screen.height;
+		float cameraHeight = camera.orthographicSize * 2;
+		Bounds bounds = new Bounds(
+			transform.position,
+			new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+		return bounds;
+	}
+	
+	public static Bounds NominalScreenAt(this Camera c, Vector3 v) {
+		var depth = v.z - c.gameObject.transform.position.z;
+		var bottomPoint = c.ScreenToWorldPoint(new Vector3(0, 0, depth));
+		var topPoint = c.ScreenToWorldPoint(new Vector3(c.pixelWidth, c.pixelHeight, depth));
+		Debug.Log(bottomPoint);
+		var bounds = new Bounds();
+		bounds.SetMinMax(bottomPoint, topPoint);
+		return bounds;
+	}
+}
---
+++ Assets/Scripts/VectorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace {
+    public static class VectorExtensions {
+        public static void ClampX(this Transform transform, float minX, float maxX) {
+            transform.position = transform.position.ClampX(minX, maxX);
+        }
+
+        public static Vector3 ClampX(this Vector3 v, float minX, float maxX) {
+            return new Vector3(Mathf.Clamp(v.x, minX, maxX), v.y, v.z);
+        }
+        
+        public static void ClampY(this Transform transform, float minY, float maxY) {
+            transform.position = transform.position.ClampY(minY, maxY);
+        }
+
+        public static Vector3 ClampY(this Vector3 v, float minY, float maxY) {
+            return new Vector3(v.x, Mathf.Clamp(v.y, minY, maxY), v.z);
+        }
+    }
+}
