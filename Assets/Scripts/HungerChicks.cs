953fab07593593f0e4cdb9875467e217dfd33fe6
jcaracciolo <jcaracciolo@itba.edu.ar>
2019-01-27 15:17:27 -0300

    Added HungerMeter

---
+++ Assets/Scripts/HungerChicks.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HungerChicks : MonoBehaviour {
+    public static HungerChicks Instance;
+
+    public float ChickHunger;
+
+    private void Awake() {
+        if (Instance != null) {
+            Destroy(Instance.gameObject);
+        }
+
+        Instance = this;
+        ChickHunger = 100;
+    }
+
+    void AddFood(float value) {
+        ChickHunger += value;
+        ChickHunger = Mathf.Clamp(ChickHunger, 0, 100);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ChickHunger -= 100/(4 * 60f) * Time.deltaTime;
+        if (ChickHunger <= 0) {
+            //TODO GameOver
+        }
+    }
+}
